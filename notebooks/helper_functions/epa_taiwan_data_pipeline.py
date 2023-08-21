4e3b2be3213dfec8dc0b18281a22f2b45480845b
amrirasyidi <amri.geodesy@gmail.com>
2023-08-21 17:19:25 +0700

    completed basic exploration

--- notebooks/helper_functions/epa_taiwan_data_pipeline.py
+++ notebooks/helper_functions/epa_taiwan_data_pipeline.py
@@ -15,16 +15,13 @@ experiment_dir = root_dir / "experiment"
 
 def import_epa_data(
     site_name:str = None,
-    year:int = None,
-    columns:List = ["SiteEngName","PM2.5","AMB_TEMP","CH4",'CO',"NMHC","read_time"]
+    year:int = None
     ) -> pd.DataFrame:
     """Read the filtered (by year and column name) csv data of epa taiwan air quality metrics
 
     Args:
         site_name (str, optional): the site to include. Defaults to None.
         year (int, optional): the year to include. Defaults to None.
-        columns (List, optional): the list of columns to include. 
-        Defaults to ["SiteEngName","PM2.5","AMB_TEMP","CH4",'CO',"NMHC","read_time"].
 
     Returns:
         pd.DataFrame: the epa taiwan data as pandas dataframe
@@ -36,10 +33,10 @@ def import_epa_data(
     if year:
         # read only specific year from the csv according to user input
         selected_year = [item for item in epa_taiwan_csv_list if str(year)[-2:] in item]
-        df = pd.read_csv(selected_year[0])[columns]
+        df = pd.read_csv(selected_year[0])
     else:
         # read all year
-        df =  pd.concat([pd.read_csv(csv) for csv in epa_taiwan_csv_list])[columns]
+        df =  pd.concat([pd.read_csv(csv) for csv in epa_taiwan_csv_list])
 
     if site_name:
         # read only specific site name
